779a55a772d51b7c221fb02bdc5b0964cc14510c
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-07-31 01:19:13 +0300

    feat(security): Add security configuration class
    This class will contain the underlining structure for the security of the API.

---
+++ src/main/java/com/banquemisr/moneytransactionservice/config/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package com.banquemisr.moneytransactionservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
