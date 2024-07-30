73f334a270ac2af8f417db8a4b9bab02ed6e9288
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-07-30 20:06:06 +0300

    feat(user): add existsByUsername and existsByEmail methods
    These 2 methods are set to boolean to check whether there's a present email or username in the database or not.

--- src/main/java/com/banquemisr/moneytransactionservice/repository/UserRepository.java
+++ src/main/java/com/banquemisr/moneytransactionservice/repository/UserRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
