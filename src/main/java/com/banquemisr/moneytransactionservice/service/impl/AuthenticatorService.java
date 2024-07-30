169c5bb759f9d3c2021ea7a0eeba14a16f4ad217
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-07-31 01:20:01 +0300

    feat(authentication): Add password hashing
    Now the password is hashed when it's saved onto the database

--- src/main/java/com/banquemisr/moneytransactionservice/service/impl/AuthenticatorService.java
+++ src/main/java/com/banquemisr/moneytransactionservice/service/impl/AuthenticatorService.java
@@ -6,24 +6,24 @@ import com.banquemisr.moneytransactionservice.exception.custom.UserAlreadyExists
 import com.banquemisr.moneytransactionservice.model.User;
 import com.banquemisr.moneytransactionservice.repository.UserRepository;
 import com.banquemisr.moneytransactionservice.service.IAuthenticator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
+@RequiredArgsConstructor
 public class AuthenticatorService implements IAuthenticator {
 
     private final UserRepository userRepository;
-
-    public AuthenticatorService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void checkIfUsernameOrEmailExists(String username, String email) {
-        if (Boolean.TRUE.equals(userRepository.existsByUsername(username))) {
+        if (Boolean.TRUE.equals(this.userRepository.existsByUsername(username))) {
             throw new UserAlreadyExistsException("This username/email already exists");
-        } else if (Boolean.TRUE.equals(userRepository.existsByEmail(email))) {
+        } else if (Boolean.TRUE.equals(this.userRepository.existsByEmail(email))) {
             throw new UserAlreadyExistsException("This username/email already exists");
         }
     }
@@ -33,7 +33,7 @@ public class AuthenticatorService implements IAuthenticator {
         return User
                 .builder()
                 .username(createUserDTO.getUsername())
-                .password(createUserDTO.getPassword())
+                .password(this.passwordEncoder.encode(createUserDTO.getPassword()))
                 .email(createUserDTO.getEmail())
                 .gender(createUserDTO.getGender())
                 .phone(createUserDTO.getPhone())
@@ -43,10 +43,10 @@ public class AuthenticatorService implements IAuthenticator {
     @Override
     @Transactional
     public UserDTO register(CreateUserDTO createUserDTO) {
-        checkIfUsernameOrEmailExists(createUserDTO.getUsername(), createUserDTO.getEmail());
+        this.checkIfUsernameOrEmailExists(createUserDTO.getUsername(), createUserDTO.getEmail());
 
-        User user = extractUserDetails(createUserDTO);
+        User user = this.extractUserDetails(createUserDTO);
 
-        return userRepository.save(user).toUserDTO();
+        return this.userRepository.save(user).toUserDTO();
     }
 }
