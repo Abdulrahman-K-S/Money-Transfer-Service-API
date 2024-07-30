1a6e66c39515f38b1cf078299163288ae60ad934
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-07-30 20:09:53 +0300

    feat(user): add IUser methods
    The methods added to the interface are:
    - checkIfUsernameOrEmailExists:
     - Which checks whether the passed username or email exist in the database. If they exists it throws a UserAlreadyExistsException and nothing if they don't.
    - extractUserDetails:
     - Which extracts the details from the passed CreateUserDTO attributes onto a User object and returns it.
    - register:
     - Which is the starting point for the register feature and it firstly invokes checkIfUsernameOrEmailExists then the extractUserDetails and returns the User object in a UserDTO object.

--- src/main/java/com/banquemisr/moneytransactionservice/service/IUser.java
+++ src/main/java/com/banquemisr/moneytransactionservice/service/IUser.java
@@ -1,4 +1,34 @@
 package com.banquemisr.moneytransactionservice.service;
 
+import com.banquemisr.moneytransactionservice.dto.CreateUserDTO;
+import com.banquemisr.moneytransactionservice.dto.UserDTO;
+import com.banquemisr.moneytransactionservice.exception.custom.UserAlreadyExistsException;
+import com.banquemisr.moneytransactionservice.model.User;
+
 public interface IUser {
+
+    /**
+     * Checks if the username or email passed are present in the database.
+     *
+     * @param username The user's username
+     * @param email The user's email
+     * @throws UserAlreadyExistsException if the username or email are already present.
+     * */
+    public void checkIfUsernameOrEmailExists(String username, String email) throws UserAlreadyExistsException;
+
+    /**
+     * Extracts the info from the createUserDTO into a User object
+     *
+     * @param createUserDTO The user's register info
+     * @return user details @{@link User}
+     * */
+    public User extractUserDetails(CreateUserDTO createUserDTO);
+
+    /**
+     * Register the user into the database.
+     *
+     * @param createUserDTO The user's register info
+     * @return registered user @{@link UserDTO}
+     * */
+    public UserDTO register(CreateUserDTO createUserDTO);
 }
