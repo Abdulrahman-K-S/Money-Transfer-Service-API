4839b07806e606c94e635d6e9e79ba533a8adab5
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-07-30 15:29:08 +0300

    feat: Add controller tags

--- src/main/java/com/banquemisr/moneytransactionservice/controller/TransactionController.java
+++ src/main/java/com/banquemisr/moneytransactionservice/controller/TransactionController.java
@@ -1,9 +1,11 @@
 package com.banquemisr.moneytransactionservice.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Transaction Controller", description = "The transaction controller endpoints")
 public class TransactionController {
 }
--- src/main/java/com/banquemisr/moneytransactionservice/controller/UserController.java
+++ src/main/java/com/banquemisr/moneytransactionservice/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.banquemisr.moneytransactionservice.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "User Controller", description = "The user controller endpoints")
 public class UserController {
 }
