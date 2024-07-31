99f8462de6078ed2963249172dfa50a4f6f1b07f
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-07-31 16:49:00 +0300

    feat(user): Add findByUsername method to User Repository

--- src/main/java/com/banquemisr/moneytransactionservice/repository/UserRepository.java
+++ src/main/java/com/banquemisr/moneytransactionservice/repository/UserRepository.java
@@ -3,9 +3,15 @@ package com.banquemisr.moneytransactionservice.repository;
 import com.banquemisr.moneytransactionservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // USER
+    Optional<User> findByUsername(String username);
+
+    // BOOLEAN
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
 }
