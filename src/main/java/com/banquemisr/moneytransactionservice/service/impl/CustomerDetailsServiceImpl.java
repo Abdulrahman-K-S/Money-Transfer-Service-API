77ce3bc2c1328e41a528376c3802de09c37e47f5
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-07-31 16:50:26 +0300

    feat(user): Add CustomerDetailsServiceImpl
    This service is responsible for implementing the UserDetailsService class which is used for the authentication

---
+++ src/main/java/com/banquemisr/moneytransactionservice/service/impl/CustomerDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.banquemisr.moneytransactionservice.service.impl;
+
+import com.banquemisr.moneytransactionservice.model.User;
+import com.banquemisr.moneytransactionservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerDetailsServiceImpl implements UserDetailsService {
+
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with username %s not found".formatted(username)));
+
+        return CustomerDetailsImpl
+                .builder()
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .build();
+    }
+}
