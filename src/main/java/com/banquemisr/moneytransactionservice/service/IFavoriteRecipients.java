5bbf3d46777b65cd6d360a70a915f5bd63088922
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-07-31 22:38:00 +0300

    feat(favorite recipient): add getAllFavoriteRecipients method signature

--- src/main/java/com/banquemisr/moneytransactionservice/service/IFavoriteRecipients.java
+++ src/main/java/com/banquemisr/moneytransactionservice/service/IFavoriteRecipients.java
@@ -1,7 +1,13 @@
 package com.banquemisr.moneytransactionservice.service;
 
 import com.banquemisr.moneytransactionservice.dto.AddFavoriteRecipientDTO;
+import com.banquemisr.moneytransactionservice.dto.UserDTO;
+import com.banquemisr.moneytransactionservice.dto.UserIdDTO;
+import com.banquemisr.moneytransactionservice.dto.ViewFavoriteRecipientDTO;
 import com.banquemisr.moneytransactionservice.model.FavoriteRecipients;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 public interface IFavoriteRecipients {
     /**
@@ -11,4 +17,12 @@ public interface IFavoriteRecipients {
      * @return The favorite recipients details @{@link FavoriteRecipients}
      * */
     public FavoriteRecipients addFavoriteRecipient(AddFavoriteRecipientDTO favoriteRecipientDTO);
+
+    /**
+     * Retrieve all the favorite recipients
+     *
+     * @param user The user ID
+     * @return A list of all the favorite recipients @{@link ViewFavoriteRecipientDTO}
+     * */
+    public List<ViewFavoriteRecipientDTO> getAllFavoriteRecipients(@RequestBody UserIdDTO user);
 }
