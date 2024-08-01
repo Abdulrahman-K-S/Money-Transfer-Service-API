84bda18822dbd58286504bdf47e1184ddeecd861
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-08-01 16:25:07 +0300

    feat(account): add createAccount method implementation

--- src/main/java/com/banquemisr/moneytransactionservice/service/impl/AccountService.java
+++ src/main/java/com/banquemisr/moneytransactionservice/service/impl/AccountService.java
@@ -4,7 +4,11 @@ import com.banquemisr.moneytransactionservice.dto.AccountDTO;
 import com.banquemisr.moneytransactionservice.exception.custom.NoTransactionsMadeException;
 import com.banquemisr.moneytransactionservice.exception.custom.NotEnoughMoneyException;
 import com.banquemisr.moneytransactionservice.exception.custom.UserNotFoundException;
+import com.banquemisr.moneytransactionservice.model.Account;
+import com.banquemisr.moneytransactionservice.repository.AccountRepository;
 import com.banquemisr.moneytransactionservice.service.IAccount;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -12,10 +16,28 @@ import java.util.Objects;
 
 
 @Service
+@AllArgsConstructor
 public class AccountService implements IAccount {
+    private final AccountRepository accountRepository;
     private HashMap<Long, Double> debit = new HashMap<>();
     private HashMap<Long, Double> credit = new HashMap<>();
     private HashMap<Long, Double> last = new HashMap<>();
+
+    @Override
+    public AccountDTO createAccount(AccountDTO accountDTO) {
+        Account account = Account
+                .builder()
+                .accountName(accountDTO.getAccountName())
+                .accountDescription(accountDTO.getAccountDescription())
+                .accountNumber(accountDTO.getAccountNumber())
+                .accountType(accountDTO.getAccountType())
+                .accountCurrency(accountDTO.getAccountCurrency())
+                .isActive(accountDTO.getIsActive())
+                .balance(accountDTO.getBalance())
+                .build();
+        return this.accountRepository.save(account).ToDTO();
+    }
+
     public AccountDTO getUserAccountBalance(Long id) throws UserNotFoundException {
         AccountDTO account = this.getUserAccountBalance(id);
 
