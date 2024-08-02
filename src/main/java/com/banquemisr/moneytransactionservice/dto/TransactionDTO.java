80abf4cd69a826be79dd82f6cb62377de56a3521
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-08-02 17:58:33 +0300

    feat(transaction): add DTO attributes and notations

--- src/main/java/com/banquemisr/moneytransactionservice/dto/TransactionDTO.java
+++ src/main/java/com/banquemisr/moneytransactionservice/dto/TransactionDTO.java
@@ -1,4 +1,16 @@
 package com.banquemisr.moneytransactionservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TransactionDTO {
+    private String fromAccountNumber;
+    private String toAccountNumber;
+    private Double amount;
 }
