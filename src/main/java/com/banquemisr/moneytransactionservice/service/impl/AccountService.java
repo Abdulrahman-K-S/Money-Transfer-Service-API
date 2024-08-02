d0e43a18759f5fb29129c4ccc9b669966c303c8f
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-08-02 18:05:42 +0300

    fix(account): change the transferMoney implementation

--- src/main/java/com/banquemisr/moneytransactionservice/service/impl/AccountService.java
+++ src/main/java/com/banquemisr/moneytransactionservice/service/impl/AccountService.java
@@ -1,26 +1,23 @@
 package com.banquemisr.moneytransactionservice.service.impl;
 
 import com.banquemisr.moneytransactionservice.dto.AccountDTO;
-import com.banquemisr.moneytransactionservice.exception.custom.NoTransactionsMadeException;
+import com.banquemisr.moneytransactionservice.dto.TransactionDTO;
+import com.banquemisr.moneytransactionservice.dto.UserTransactionDTO;
 import com.banquemisr.moneytransactionservice.exception.custom.NotEnoughMoneyException;
 import com.banquemisr.moneytransactionservice.exception.custom.UserNotFoundException;
 import com.banquemisr.moneytransactionservice.model.Account;
+import com.banquemisr.moneytransactionservice.model.Transaction;
 import com.banquemisr.moneytransactionservice.repository.AccountRepository;
 import com.banquemisr.moneytransactionservice.service.IAccount;
+import com.banquemisr.moneytransactionservice.service.ITransaction;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Objects;
-
 
 @Service
 @AllArgsConstructor
 public class AccountService implements IAccount {
     private final AccountRepository accountRepository;
-    private HashMap<Long, Double> debit = new HashMap<>();
-    private HashMap<Long, Double> credit = new HashMap<>();
-    private HashMap<Long, Double> last = new HashMap<>();
+    private final ITransaction transactionService;
 
     @Override
     public AccountDTO createAccount(AccountDTO accountDTO) {
@@ -37,38 +34,26 @@ public class AccountService implements IAccount {
         return this.accountRepository.save(account).ToDTO();
     }
 
+    @Override
     public double getUserAccountBalance(Long accountNumber) throws UserNotFoundException {
         Account account = this.accountRepository.findById(accountNumber).orElseThrow(UserNotFoundException::new);
         return account.getBalance();
     }
 
-    public void transferMoney(Long fromAccountNumber,Long toAccountNumber,double balance) throws UserNotFoundException , NotEnoughMoneyException {
-        Account fromAccount = this.accountRepository.findById(fromAccountNumber).orElseThrow(UserNotFoundException::new);
-        Account toAccount = this.accountRepository.findById(toAccountNumber).orElseThrow(UserNotFoundException::new);
+    @Override
+    public UserTransactionDTO transferMoney(TransactionDTO transactionDTO) throws UserNotFoundException , NotEnoughMoneyException {
+        Account fromAccount = this.accountRepository.findByAccountNumber(transactionDTO.getFromAccountNumber()).orElseThrow(UserNotFoundException::new);
+        Account toAccount = this.accountRepository.findByAccountNumber(transactionDTO.getToAccountNumber()).orElseThrow(UserNotFoundException::new);
 
-        if (fromAccount.getBalance() < balance) {
+        if (fromAccount.getBalance() < transactionDTO.getAmount()) {
             throw new NotEnoughMoneyException();
         }
-        else {
-            fromAccount.setBalance(fromAccount.getBalance() - balance);
-            toAccount.setBalance(toAccount.getBalance() + balance);
-            this.accountRepository.save(fromAccount);
-            this.accountRepository.save(toAccount);
-        }
-    }
+        fromAccount.setBalance(fromAccount.getBalance() - transactionDTO.getAmount());
+        toAccount.setBalance(toAccount.getBalance() + transactionDTO.getAmount());
+        this.accountRepository.save(fromAccount);
+        this.accountRepository.save(toAccount);
 
-    public HashMap<Long, Double> transactionHistory(Long accountNumber) throws UserNotFoundException,NoTransactionsMadeException {
-        last.clear();
-        debit.forEach((key, value) -> {
-            if (Objects.equals(key, accountNumber)) {
-                last.put(accountNumber, value);
-            }
-        });
-        credit.forEach((key, value) -> {
-            if (Objects.equals(key, accountNumber)) {
-                last.put(accountNumber, value);
-            }
-        });
-        return last;
+        Transaction transaction = this.transactionService.createTransaction(fromAccount, toAccount, transactionDTO.getAmount());
+        return this.transactionService.addToTransactionHistory(transaction, fromAccount.getUser());
     }
 }
