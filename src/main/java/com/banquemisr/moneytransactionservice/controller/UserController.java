5737b6aeff064ec1e176cfd2da4552ba2f1788df
Mohamed Ahmed El Sawy <m.elsawy2003@gmail.com>
2024-08-05 13:28:21 +0300

    change the currency

--- src/main/java/com/banquemisr/moneytransactionservice/controller/UserController.java
+++ src/main/java/com/banquemisr/moneytransactionservice/controller/UserController.java
@@ -10,12 +10,33 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
     public Country country;
 
-    public double getRate(String currency, int amount) {
+    public double getRateToEGP(String currency, double amount) {
 
         CurrencyRate currencyRate = CurrencyRate.fromCurrencyCode(currency);
         assert currencyRate != null;
         double rate = currencyRate.getRate();
             return rate * amount;
 
+    }
+    public double getRateFromEGP(String currency, double amount) {
+
+        CurrencyRate currencyRate = CurrencyRate.fromCurrencyCode(currency);
+        assert currencyRate != null;
+        double rate = currencyRate.getRate();
+        return amount / rate;
+
+    }
+    public double getRate(String fromCurrency, String toCurrency , double amount) {
+
+//        CurrencyRate fromCurrencyRate = CurrencyRate.fromCurrencyCode(fromCurrency);
+//        CurrencyRate toCurrencyRate = CurrencyRate.fromCurrencyCode(toCurrency);
+//        assert fromCurrencyRate != null;
+//        assert toCurrencyRate != null;
+//        double fromRate = fromCurrencyRate.getRate();
+//        double toRate = toCurrencyRate.getRate();
+//        double temp = fromRate * amount;
+//        return temp / toRate;
+        double from = getRateToEGP(fromCurrency,amount);
+        return getRateFromEGP(toCurrency,from);
     }
 }
