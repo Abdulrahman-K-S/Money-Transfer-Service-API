a453364734ca583e635ad6d5415e125793cab6b1
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-08-05 16:37:28 +0300

    refactor: remove redundant attribute

--- src/main/java/com/banquemisr/moneytransactionservice/dto/LoginRequestDTO.java
+++ src/main/java/com/banquemisr/moneytransactionservice/dto/LoginRequestDTO.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequestDTO {
-    private String username;
     private String email;
     private String password;
 }
