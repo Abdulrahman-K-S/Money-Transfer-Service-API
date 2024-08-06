c5ac698a4bc8280c97b0307e95c89301d52c6163
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-08-06 09:37:28 +0300

    refactor: add ApiResponse notations and enhanced their descriptions

--- src/main/java/com/banquemisr/moneytransactionservice/controller/AccountController.java
+++ src/main/java/com/banquemisr/moneytransactionservice/controller/AccountController.java
@@ -27,6 +27,8 @@ public class AccountController {
     private final JwtUtils jwtUtils;
 
     @PostMapping("/create_account")
+    @ApiResponse(responseCode = "201", description = "Account creation successful", content = {@Content(schema = @Schema(implementation = AccountDTO.class), mediaType = "application/json")})
+    @ApiResponse(responseCode = "409", description = "Account already exists", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")})
     public ResponseEntity<AccountDTO> createAccount(@RequestHeader("Authorization") String authorizationHeader, @RequestBody AccountDTO accountDTO) {
         String email = jwtUtils.getEmailFromHeader(authorizationHeader);
         return new ResponseEntity<>(
@@ -36,12 +38,11 @@ public class AccountController {
     }
 
     @Operation(summary = "Get user Account Balance")
-    @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = UserDTO.class), mediaType = "application/json")})
-    @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")})
+    @ApiResponse(responseCode = "200", description = "Account's balance retrieved successfully", content = {@Content(schema = @Schema(implementation = Double.class), mediaType = "application/json")})
+    @ApiResponse(responseCode = "404", description = "Account not found", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")})
     @GetMapping("/balance")
     public ResponseEntity<Double> getUserAccountBalance(@RequestHeader(value = "Authorization") String authorizationHeader, @RequestBody AccountNumberDTO accountDTO) throws UserNotFoundException {
-        String token = jwtUtils.extractTokenFromHeader(authorizationHeader);
-        String email = jwtUtils.getUserNameFromJwtToken(token);
+        String email = jwtUtils.getEmailFromHeader(authorizationHeader);
         return new ResponseEntity<>(
                 accountService.getUserAccountBalance(accountDTO.getAccountNumber(), email),
                 HttpStatus.OK
@@ -49,8 +50,9 @@ public class AccountController {
     }
 
     @Operation(summary = "Transfer money to another account and create a transaction ticket")
-    @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = UserDTO.class), mediaType = "application/json")})
-    @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")})
+    @ApiResponse(responseCode = "200", description = "Money transferred successfully", content = {@Content(schema = @Schema(implementation = UserTransactionDTO.class), mediaType = "application/json")})
+    @ApiResponse(responseCode = "400", description = "Account not expired/inactive", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")})
+    @ApiResponse(responseCode = "404", description = "Account not found", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")})
     @PostMapping("/transfer")
     public ResponseEntity<UserTransactionDTO> transferMoney(@RequestBody TransactionDTO transactionDTO) throws UserNotFoundException, NotEnoughMoneyInAccountException, AccountNotFoundException, AccountNotActiveException {
         return new ResponseEntity<>(
--- src/main/java/com/banquemisr/moneytransactionservice/controller/UserController.java
+++ src/main/java/com/banquemisr/moneytransactionservice/controller/UserController.java
@@ -3,6 +3,10 @@ package com.banquemisr.moneytransactionservice.controller;
 import com.banquemisr.moneytransactionservice.dto.CurrencyRateDTO;
 import com.banquemisr.moneytransactionservice.dto.CurrencyToFromRateDTO;
 import com.banquemisr.moneytransactionservice.service.IUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,6 +24,8 @@ public class UserController {
 
     private final IUser userService;
 
+    @Operation(summary = "Changes rate to EGP")
+    @ApiResponse(responseCode = "200", description = "Rate changed successfully", content = {@Content(schema = @Schema(implementation = Double.class), mediaType = "application/json")})
     @GetMapping("/ratetoEGP")
     public ResponseEntity<Double> getRateToEGP(@RequestBody CurrencyRateDTO currencyRateDTO) {
         return new ResponseEntity<>(
@@ -28,6 +34,8 @@ public class UserController {
         );
     }
 
+    @Operation(summary = "Changes rate from EGP")
+    @ApiResponse(responseCode = "200", description = "Rate changed successfully", content = {@Content(schema = @Schema(implementation = Double.class), mediaType = "application/json")})
     @GetMapping("/ratefromEGP")
     public ResponseEntity<Double> getRateFromEGP(@RequestBody CurrencyRateDTO currencyRateDTO) {
         return new ResponseEntity<>(
@@ -36,6 +44,8 @@ public class UserController {
         );
     }
 
+    @Operation(summary = "Changes rate to inputted currencies")
+    @ApiResponse(responseCode = "200", description = "Rate changed successfully", content = {@Content(schema = @Schema(implementation = Double.class), mediaType = "application/json")})
     @GetMapping("/rate")
     public ResponseEntity<Double> getRate(@RequestBody CurrencyToFromRateDTO currencyToFromRateDTO) {
         return new ResponseEntity<>(
