1e0b986fc8fc1e95896ae168d66d7c6112e7c1b1
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-08-06 11:22:15 +0300

    feat(exception): add RefreshTokenNotFoundException and RefreshTokenExpiredException to global exception handler

--- src/main/java/com/banquemisr/moneytransactionservice/exception/MoneyTransferServiceExceptionHandler.java
+++ src/main/java/com/banquemisr/moneytransactionservice/exception/MoneyTransferServiceExceptionHandler.java
@@ -28,6 +28,12 @@ public class MoneyTransferServiceExceptionHandler {
         return new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
     }
 
+    @ExceptionHandler(value = RefreshTokenNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public @ResponseBody ErrorResponse handleException(RefreshTokenNotFoundException ex) {
+        return new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+    }
+
     @ExceptionHandler(value = UserAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public @ResponseBody ErrorResponse handleException(UserAlreadyExistsException ex) {
@@ -67,6 +73,12 @@ public class MoneyTransferServiceExceptionHandler {
     @ExceptionHandler(value = AccountExpiredException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public @ResponseBody ErrorResponse handleException(AccountExpiredException ex) {
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    }
+
+    @ExceptionHandler(value = RefreshTokenExpiredException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public @ResponseBody ErrorResponse handleException(RefreshTokenExpiredException ex) {
         return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
     }
 
