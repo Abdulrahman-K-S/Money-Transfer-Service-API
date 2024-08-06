7823c38052cc8ebec487b0cdb627be6faa7d47ae
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-08-06 11:23:08 +0300

    feat(utils): add generateToken by email and createToken methods to JwtUtils

--- src/main/java/com/banquemisr/moneytransactionservice/utils/JwtUtils.java
+++ src/main/java/com/banquemisr/moneytransactionservice/utils/JwtUtils.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -34,6 +36,20 @@ public class JwtUtils {
                 .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
                 .signWith(key(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public String generateToken(String username){
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+    public String createToken(Map<String, Object> claims, String username) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(key(), SignatureAlgorithm.HS256).compact();
     }
 
     private Key key() {
