0591257176c1ae39d2529335a300f6ca00e3b258
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-08-07 01:00:52 +0300

    feat: add @CrossOrigin to all controllers

--- src/main/java/com/banquemisr/moneytransactionservice/controller/FavoriteRecipientsController.java
+++ src/main/java/com/banquemisr/moneytransactionservice/controller/FavoriteRecipientsController.java
@@ -24,6 +24,7 @@ import java.util.List;
         name = "Favorite Recipient Controller",
         description = "The controller that's responsible for the recipient favorites"
 )
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
--- src/main/java/com/banquemisr/moneytransactionservice/controller/UserController.java
+++ src/main/java/com/banquemisr/moneytransactionservice/controller/UserController.java
@@ -7,16 +7,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@Getter
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
